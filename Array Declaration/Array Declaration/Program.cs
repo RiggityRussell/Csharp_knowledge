c942d64d01eb9f19d613861b50ccb08f3b3f6856
RiggityRussell <77250752+RiggityRussell@users.noreply.github.com>
2022-02-22 08:55:37 -0500

    Add files via upload

---
+++ Array Declaration/Array Declaration/Program.cs
@@ -0,0 +1,6 @@
+﻿
+
+
+
+int[] numbers = new int[1]; //this declares an array with a single int that can be stored in it.
+int[] numbers2 = { 1, 2, 3};  //this declares an array with hard coded ints inside it.
